75520a204226b8732ffaaf33df4988f3e2169f81
Elyes Mansour <elyesmansour@gmail.com>
2018-11-24 22:53:21 +0100

    changed minsdk to 21 started on login screen

--- app/src/main/java/tn/duoes/esprit/cookmania/controllers/activities/MainActivity.java
+++ app/src/main/java/tn/duoes/esprit/cookmania/controllers/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package tn.duoes.esprit.cookmania.controllers.activities;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        TextInputLayout loginInputLayout = findViewById(R.id.login_input_layout);
     }
 }
